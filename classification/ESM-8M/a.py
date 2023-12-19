353868f81fec7a18c47356d5b0cba62ffcffc50e
Oliver <1527523751@qq.com>
2023-12-19 19:51:51 +0800

    1

--- classification/ESM-8M/a.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoTokenizer, EsmModel
-import torch
-
-tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t6_8M_UR50D",cache_dir='./')
-model = EsmModel.from_pretrained("facebook/esm2_t6_8M_UR50D",cache_dir='./')
-
-print(model)
